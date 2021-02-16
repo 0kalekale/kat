f4df64cbd91a2dd30ffe80559878cba7bad82f90
0kalekale <kalekale.anon@gmail.com>
2021-02-16 20:16:25 +0545

    Added input from standard input -h and --help flags

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{self, Read};
 
 fn main(){
     let args: Vec<String> = env::args().collect();
     let num = args.len();
-    
     if num == 1{
-        println!("not enough arguemnts! see -h for more");
-    }
-    
-    else {
-    
-        if args[1] == "-h"{
-        
+        readstdin();
+    }    
+    else {    
+        if args[1] == "-h" || args[1] == "--help" {
             println!("Kat - Koncatenate FILE(s) to standard output.\n");
-            println!("Usage: kat [FILE]s...");
-            
+            println!("Usage: kat [FILE]s... 'OR' kat from standard input");
         }
-        
         else{
             for i in 1..num{
                 let arg1 = args[i].clone();
@@ -33,5 +27,12 @@ pub fn readfile(filename: String) -> std::io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     println!("{}", contents);
+    Ok(())
+}
+pub fn readstdin() -> io::Result<()>{
+    let mut buffer = String::new();
+    let mut stdin = io::stdin();
+    stdin.read_to_string(&mut buffer)?;
+    println!("{}", buffer);
     Ok(())
 }
